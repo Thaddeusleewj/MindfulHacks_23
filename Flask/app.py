27f8c4e2fae31a2e7a877ee4a99254eceb96a1b2
jiayi <tangjiayi64@gmail.com>
2023-10-07 18:28:15 +0800

    openai post

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import openai
 
 app = Flask(__name__)
 
@@ -6,8 +7,20 @@ app = Flask(__name__)
 def hello_world():
     return 'hello world'
 
+#post request to use whisper api to transcript audio 
+@app.route('/transcript', methods=['POST'])
+def transcript():
+    files = request.files
+    file = files.get('file')
+    with open("temp.mp3", "wb") as f:
+        f.write(file)
+        f.close()
+        
+    audio_file= open("temp.mp3", "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    
+    return transcript
 
-@
 
 if __name__ == '__main__':
     app.run(debug=True)
