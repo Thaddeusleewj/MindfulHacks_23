c945d69c6bdf29fb465c73b4924a7fcfdcde87a8
Thaddeus Lee <thaddeuslee@Thaddeuss-MacBook-Pro.local>
2023-10-08 10:33:17 +0800

    Finished Report Outline

--- Frontend/src/pages/Report.jsx
+++ Frontend/src/pages/Report.jsx
@@ -1,45 +1,39 @@
 import React from "react";
-import {
-  Chart as ChartJS,
-  RadialLinearScale,
-  PointElement,
-  LineElement,
-  Filler,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import { Radar } from "react-chartjs-2";
 
-ChartJS.register(
-  RadialLinearScale,
-  PointElement,
-  LineElement,
-  Filler,
-  Tooltip,
-  Legend
-);
+import Navbar from "../components/Navbar";
 
-export const data = {
-  labels: ["Relationships", "Addictions", "Trauma", "Life Transitions", "Self Esteem", "Spiritual Concerns"],
-  datasets: [
-    {
-      label: "# of Votes",
-      data: [5, 9, 3, 5, 2, 3],
-      backgroundColor: "rgba(255, 99, 132, 0.2)",
-      borderColor: "rgba(255, 99, 132, 1)",
-      borderWidth: 1,
-    },
-  ],
-};
+import RadarGraph from "../components/RadarGraph";
+import LineGraph from "../components/LineGraph";
+import TxtView from "../components/TxtView";
 
 const Report = () => {
   return (
-    <div className="flex w-full m-16">
-        <div className="aspect-square w-1/2 border-4 border-sky-400 rounded-lg p-5">
-            <Radar data={data} />
+    <>
+      <div className="flex w-full">
+        <Navbar />
+        <div className="flex flex-col w-full m-5">
+          <div className="flex h-1/2 items-center">
+            <RadarGraph />
+            <LineGraph />
+          </div>
+          <div className="h-1/2">
+            <div className="flex aspect-video w-screen bg-blue-400 rounded-xl p-3">
+              <div className="w-3/4 aspect-video bg-black rounded-xl mr-5"></div>
+              <div className="flex flex-col justify-around aspect-[1/3] w-1/4 max-h-full space-y-2 overflow-scroll">
+                <div className="aspect-[2/1] w-full bg-white rounded-lg"></div>
+                <div className="aspect-[2/1] w-full bg-white rounded-lg"></div>
+                <div className="aspect-[2/1] w-full bg-white rounded-lg"></div>
+                <div className="aspect-[2/1] w-full bg-white rounded-lg"></div>
+                <div className="aspect-[2/1] w-full bg-white rounded-lg"></div>
+                <div className="aspect-[2/1] w-full bg-white rounded-lg"></div>
+              </div>
+            </div>
+            <TxtView />
+          </div>
         </div>
-    </div>
-  )
+      </div>
+    </>
+  );
 };
 
 export default Report;
